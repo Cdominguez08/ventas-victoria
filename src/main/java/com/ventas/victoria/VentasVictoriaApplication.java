8f5d7dc92a821d67e65f5d94aa3580d0b9205c49
Cesar Dominguez <cesar.dominguez08@gmail.com>
2021-01-02 14:36:19 -0600

    First Commit.

---
+++ src/main/java/com/ventas/victoria/VentasVictoriaApplication.java
@@ -0,0 +1,13 @@
+package com.ventas.victoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasVictoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasVictoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventas/victoria/VentasVictoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.victoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasVictoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
